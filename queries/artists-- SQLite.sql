b4d33ed324a176fe17611a1c0ea0b6288f7dd5ea
ayumitanaka <tanakayumi13@gmail.com>
2020-12-30 16:54:46 -0800

    add images

--- queries/artists-- SQLite.sql
+++ queries/artists-- SQLite.sql
@@ -12,14 +12,14 @@ INSERT INTO artists (id, year, name, name_j, title, title_j, youtube, song_info,
 VALUES (2, 1980, 'Tatsuro Yamashita', '山下 達郎', 'Ride On Time', 'ライド・オン・タイム', 'https://www.youtube.com/embed/1rdlHKioR6A',
         'Tatsuro`s first big hit, a very popular and iconic city pop song! You can feel the uplifting feeling from the start of the song.
          Not only Tatsuro`s voice, but also Minako Yoshida`s chorus is wonderful. And do not miss the soul-style slow number on the B-side, "Rainy Walk".',
-        'Yamashita Tatsuro (February 4, 1953) is a Japanese singer-songwriter and record producer
+        'Tatsuro Yamashita (February 4, 1953) is a Japanese singer-songwriter and record producer
          who helped pioneer the style of Japanese adult-oriented rock/soft rock dubbed "city pop". He has also collaborated with his wife Mariya Takeuchi.',
-        'image/s2.jpg', 'image/p-yamashita.jpg', 3);
+        'image/s2.jpg', 'image/p-yamashita.jpg', 2);
 
 INSERT INTO artists (id, year, name, name_j, title, title_j, youtube, song_info, artist_info, song_picture_path, artist_picture_path, from_album_id)
-VALUES (4, 1980, 'Mariya Takeuchi', '竹内まりや', 'Ride On Time', 'ライド・オン・タイム', 'https://www.youtube.com/embed/1rdlHKioR6A',
-        'Tatsuro`s first big hit, a very popular and iconic city pop song! You can feel the uplifting feeling from the start of the song.
-         Not only Tatsuro`s voice, but also Minako Yoshida`s chorus is wonderful. And do not miss the soul-style slow number on the B-side, "Rainy Walk".',
-        'Yamashita Tatsuro (February 4, 1953) is a Japanese singer-songwriter and record producer
-         who helped pioneer the style of Japanese adult-oriented rock/soft rock dubbed "city pop". He has also collaborated with his wife Mariya Takeuchi.',
-        'image/s2.jpg', 'image/p-yamashita.jpg', 3);
+VALUES (3, 1984, 'Mariya Takeuchi', '竹内まりや', 'Plastic Love', 'プラスティック・ラブ', 'https://www.youtube.com/embed/3bNITQR4Uso',
+        'This is the most popular Japanese city pop song! This song was produced by her husband Tatsuro Yamashita. He also has covered her song.
+         She sings about a lonely woman who can not fall in love for real because of her past love. I like the sadness of this song.',
+        'Mariya Takeuchi (March 20, 1955) is a Japanese singer-songwriter. In a Japanese pop music career spanning decades,
+         Takeuchi is one of the best-selling music artists in Japan. Her early work during the late 70s to 80s is largely associated with the Japanese city pop.',
+        'image/s3.jpg', 'image/p-takeuchi.jpg', 3);
