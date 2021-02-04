61b523cf43b2b5eb70a1a3941ad79671a71200e9
ayumitanaka <tanakayumi13@gmail.com>
2021-02-03 20:27:27 -0800

    added package.json

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        lightpink: '#ff93cd',
+        lightblue: '#3dbaf4'
+      },
+      fontFamily: {
+        codystar: ['Codystar', 'cursive'],
+        poppins: ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
