04e697b3bfef804171765076e883204eef846367
ayumitanaka <tanakayumi13@gmail.com>
2021-02-24 17:49:42 -0800

    modified __init__.py

--- db_create.py
+++ db_create.py
@@ -1,3 +1,3 @@
-from app import db
+# from app import db
 
-db.create_all()
+# db.create_all()
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -15,11 +15,11 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    # app.config['SECRET_KEY'] = 'mysite'
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = \
-    #     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+    app.config['SECRET_KEY'] = 'mysite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from flaskr.history.views import history_bp
     from flaskr.artist.views import artist_bp
